4b870b9ed999e89a4118f6ffecc3c4a76aa59ac9
Anthony <asciola93@gmail.com>
2024-01-14 10:33:06 -0700

    Adding qol keymaps for git conflict resolution

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,1 +1,9 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git, {desc="Git control"})
+
+vim.keymap.set("n", "<leader>dg3", function()
+    vim.cmd.diffget('//3')
+end, {desc="Apply changes from merging branch"})
+
+vim.keymap.set("n", "<leader>dg2", function()
+    vim.cmd.diffget('//2')
+end, {desc="Apply changes from base branch"})
